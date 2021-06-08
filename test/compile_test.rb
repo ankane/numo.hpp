d507c2c3ac8b2d40e507cd4f361963bd1a3520b5
Andrew Kane <andrew@ankane.org>
2021-06-08 02:10:34 -0700

    Improved tests

--- test/compile_test.rb
+++ test/compile_test.rb
@@ -2,28 +2,38 @@ require_relative "test_helper"
 
 class CompileTest < Minitest::Test
   def test_rank1
-    assert_equal [2], Hello.rank1.shape
-    assert_equal 2, Hello.rank1.size
+    x = Hello.rank1
+    assert_equal [2], x.shape
+    assert_equal 2, x.size
+    assert_equal 1, x.ndim
   end
 
   def test_rank1_dynamic
-    assert_equal [2], Hello.rank1_dynamic.shape
-    assert_equal 2, Hello.rank1_dynamic.size
+    x = Hello.rank1_dynamic
+    assert_equal [2], x.shape
+    assert_equal 2, x.size
+    assert_equal 1, x.ndim
   end
 
   def test_rank2
-    assert_equal [2, 3], Hello.rank2.shape
-    assert_equal 6, Hello.rank2.size
+    x = Hello.rank2
+    assert_equal [2, 3], x.shape
+    assert_equal 6, x.size
+    assert_equal 2, x.ndim
   end
 
   def test_rank2_dynamic
-    assert_equal [2, 3], Hello.rank2_dynamic.shape
-    assert_equal 6, Hello.rank2_dynamic.size
+    x = Hello.rank2_dynamic
+    assert_equal [2, 3], x.shape
+    assert_equal 6, x.size
+    assert_equal 2, x.ndim
   end
 
   def test_rank3
-    assert_equal [2, 3, 4], Hello.rank3.shape
-    assert_equal 24, Hello.rank3.size
+    x = Hello.rank3
+    assert_equal [2, 3, 4], x.shape
+    assert_equal 24, x.size
+    assert_equal 3, x.ndim
   end
 
   def test_return_object
