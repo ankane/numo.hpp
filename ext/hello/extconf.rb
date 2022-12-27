fd3461e3b69c9a21e4e8d48a4071be8d9f2e1635
Andrew Kane <andrew@ankane.org>
2022-12-27 14:27:18 -0800

    Fixed compilation with Apple Clang 14

--- ext/hello/extconf.rb
+++ ext/hello/extconf.rb
@@ -5,6 +5,9 @@ numo = File.join(Gem.loaded_specs["numo-narray"].require_path, "numo")
 abort "Numo header not found" unless find_header("numo/narray.h", numo)
 abort "Numo library not found" if Gem.win_platform? && !find_library("narray", nil, numo)
 
+# for https://bugs.ruby-lang.org/issues/19005
+$LDFLAGS += " -Wl,-undefined,dynamic_lookup" if RbConfig::CONFIG["host_os"] =~ /darwin/i
+
 find_header("numo.hpp", File.expand_path("../../include", __dir__))
 
 create_makefile("hello")
