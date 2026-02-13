91285fd7adeee6b7eafa142ae89daecccad4915b
Andrew Kane <andrew@ankane.org>
2026-02-13 02:54:21 -0800

    Fixed CI

--- ext/hello/extconf.rb
+++ ext/hello/extconf.rb
@@ -3,7 +3,7 @@ require "numo/narray"
 
 numo = File.join(Gem.loaded_specs["numo-narray-alt"].require_path, "numo")
 abort "Numo header not found" unless find_header("numo/narray.h", numo)
-abort "Numo library not found" if Gem.win_platform? && !find_library("narray", nil, numo)
+abort "Numo library not found" if Gem.win_platform? && !find_library("narray", nil, "#{numo}/narray")
 $LDFLAGS += " -Wl,-undefined,dynamic_lookup" if RbConfig::CONFIG["host_os"] =~ /darwin/i
 
 find_header("numo.hpp", File.expand_path("../../include", __dir__))
