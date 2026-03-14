04a78dd3afbe4c512b9af75867aec60e968bd104
Andrew Kane <andrew@ankane.org>
2026-03-13 18:51:33 -0700

    Updated Rice

--- Gemfile
+++ Gemfile
@@ -4,5 +4,5 @@ gem "rake"
 gem "rake-compiler"
 gem "minitest"
 gem "numo-narray-alt"
-gem "rice", ENV["RICE_VERSION"] || ">= 4.5", require: false
+gem "rice", github: "ruby-rice/rice", ref: "e74aa875461e1981ce40fba55a4819c29c998f0a"
 gem "ruby_memcheck"
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,3 +3,14 @@ Bundler.require(:default)
 require "minitest/autorun"
 
 require_relative "../lib/hello"
+
+class Minitest::Test
+  def setup
+    puts "#{self.class.name}##{name}"
+    GC.stress = true
+  end
+
+  def teardown
+    GC.stress = false
+  end
+end
